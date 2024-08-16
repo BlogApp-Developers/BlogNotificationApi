ffcf44cf4b9f78a906f39fa5162f01d0c8442c32
Emil B <emkaran_500@mail.ru>
2024-08-16 15:12:25 +0400

    Add Jwt Authorization

--- src/Controllers/NotificationController.cs
+++ src/Controllers/NotificationController.cs
@@ -7,41 +7,73 @@ using Microsoft.AspNetCore.Components;
 using BlogNotificationApi.Notification.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+using BlogNotificationApi.Methods;
+using Microsoft.Extensions.Primitives;
 
 [Route("api/[controller]")]
 [ApiController]
 public class NotificationsController : ControllerBase
 {
     private readonly NotificationsDbContext dbContext;
+    private readonly TokenValidation tokenValidation;
 
-    public NotificationsController(NotificationsDbContext dbContext)
+    public NotificationsController(NotificationsDbContext dbContext, TokenValidation tokenValidation)
     {
         this.dbContext = dbContext;
+        this.tokenValidation = tokenValidation;
     }
 
-    [Authorize]
     [HttpGet("{userId}")]
     public async Task<ActionResult<IEnumerable<Notification>>> GetUserNotifications(Guid userId)
     {
+        try
+        {
+            base.HttpContext.Request.Headers.TryGetValue("Bearer", out StringValues headerValues);
+            var tokenNew = headerValues.FirstOrDefault();
+            this.tokenValidation.ValidateToken(tokenNew);
+        }
+        catch (Exception ex)
+        {
+            return Unauthorized(ex.Message);
+        }
+
         return await this.dbContext.Notifications
             .Where(n => n.UserId == userId)
             .OrderByDescending(n => n.CreatedAt)
             .ToListAsync();
     }
 
-    [Authorize]
     [HttpPost]
     public async Task<ActionResult<Notification>> CreateNotification(Notification notification)
     {
+        try
+        {
+            var tokenNew = base.HttpContext.Request.Headers["Bearer"][0];
+            this.tokenValidation.ValidateToken(tokenNew);
+        }
+        catch (Exception ex)
+        {
+            return Unauthorized(ex.Message);
+        }
+
         this.dbContext.Notifications.Add(notification);
         await this.dbContext.SaveChangesAsync();
         return CreatedAtAction(nameof(GetUserNotifications), new { userId = notification.UserId }, notification);
     }
 
-    [Authorize]
     [HttpPut("{id}")]
     public async Task<IActionResult> MarkAsRead(Guid id)
     {
+        try
+        {
+            var tokenNew = base.HttpContext.Request.Headers["Bearer"][0];
+            this.tokenValidation.ValidateToken(tokenNew);
+        }
+        catch (Exception ex)
+        {
+            return Unauthorized(ex.Message);
+        }
+
         var notification = await this.dbContext.Notifications.FindAsync(id);
         if (notification == null) 
             return NotFound();
---
+++ src/Methods/TokenValidation.cs
@@ -0,0 +1,42 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Principal;
+using BlogNotificationApi.Options;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BlogNotificationApi.Methods;
+
+public class TokenValidation
+{
+    private readonly JwtOptions jwtOptions;
+
+    public TokenValidation(IOptionsSnapshot<JwtOptions> jwtOptionsSnapshot)
+    {
+        this.jwtOptions = jwtOptionsSnapshot.Value;
+    }
+    public bool ValidateToken(string authToken)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var validationParameters = GetValidationParameters();
+
+        SecurityToken validatedToken;
+        IPrincipal principal = tokenHandler.ValidateToken(authToken, validationParameters, out validatedToken);
+        return true;
+    }
+
+    public TokenValidationParameters GetValidationParameters()
+    {
+       
+        return new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = jwtOptions.Issuer,
+
+            ValidateAudience = true,
+            ValidAudience = jwtOptions.Audience,
+
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(jwtOptions.KeyInBytes)
+        };
+    }
+}
