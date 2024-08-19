c83b66218885dc5498222246bb1b3e3ec69461eb
Emil B <emkaran_500@mail.ru>
2024-08-20 00:32:14 +0400

    Add email Notifications

---
+++ src/User/Models/User.cs
@@ -0,0 +1,6 @@
+namespace BlogNotificationApi.User.Models;
+
+public class User
+{
+    public string Email { get; set; }
+}
---
+++ src/User/Repositories/Base/IUserRepository.cs
@@ -0,0 +1,8 @@
+namespace BlogNotificationApi.User.Repositories.Base;
+
+using BlogNotificationApi.User.Models;
+
+public interface IUserRepository
+{
+    Task<User> GetByIdAsync(Guid id);
+}
